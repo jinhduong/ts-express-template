ff3d5ab853a6c5b6d5a5db35c74ed453672a0863
Dinh Duong <0961020@gmail.com>
2018-09-14 10:33:08 +0800

    Add express web layout by hbs engine

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
+import * as  exphbs from'express-handlebars';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.set('view engine', '.hbs');
+app.engine('.hbs', exphbs({ defaultLayout: 'layout', extname: '.hbs' }));
 
-app.get('/', async (req, res) => {
-    res.json('Hello world');
+
+app.get('/', (req, res) => {
+    res.render('index');
 })
 
 app.listen(port, () => console.log(`Server running at ${port}`));
